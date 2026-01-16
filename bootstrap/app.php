37fb2de50fd54f72256869b0a0e9d6acd285ac2d
A.R.A.M.Gunarathna <gunarathnaasirimahesh@gmail.com>
2026-01-16 23:42:02 +0530

    refactor: clean up whitespace and remove trustProxies middleware from app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -21,13 +21,6 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        $middleware->trustProxies(
-            '*',
-            Request::HEADER_X_FORWARDED_HOST |
-                Request::HEADER_X_FORWARDED_PORT |
-                Request::HEADER_X_FORWARDED_PROTO
-        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
